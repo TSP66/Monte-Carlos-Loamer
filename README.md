9d4592b87f663311c28d495d25a6e49029fd8865
TSP66 <q.mot.tom.p@gmail.com>
2024-04-02 21:39:50 +1100

    Added README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Monte Carlos Loamer
- 
+
+> __Loaming: "A method of geochemical prospecting in which samples of soil or other surficial material are tested for traces of the metal desired, its presence presumably indicating a near-surface orebody." - The American Geosciences Institute__
+
+This is a Julia tool that employs [Monte Carlo methods](https://en.wikipedia.org/wiki/Monte_Carlo_method) to simulate the spread of geochemical anomalies a mineral deposit could present on a mountain side. It is desgined with the gold prospector in mind - able to simulate loaming (samples to likely deposit) and reverse-loaming (deposit to likely samples), maximising the theoretical chances of finding a 'patch' using what very limited information may be avaliable. It is currently able to model 'point' outcrops and lode/vein outcrops (including dip, strike and length). 
+
+Functionality is being added to, suggestions are welcome.
--- sim.jl
+++ sim.jl
@@ -26,21 +26,21 @@ elv_data = reshape(elv_data,2911,7207)#4200,3600)
 
 #1587
 #1108
-X = 3587
-Y = 1108
+X = 4087
+Y = 1558
 
-res = 300
+res = 550
 
 elv_data = elv_data[Y-res:Y+res,X-res:X+res]
 
 #heat_map = monte_carlo(201,201,elv_data,arc_probs,30000,true)
-heat_map = revLoam(Sample(res+1,res+1,"-",0.0),elv_data,arc_probs,100000)
+#heat_map = revLoam(Sample(res+1,res+1,"-",0.0),elv_data,arc_probs,10000)
 #heat_map = Loam([Sample(res+25,res+25,"-",1), Sample(res+30,res+50,"-",1)],elv_data,arc_probs,250000,true)
 #Reef = reef(res+40,res+40,190,175,"Normal")
 
-#Reef = reef(res-20,res-30,70,250,35,elv_data,"-")
+Reef = reef(res-20,res-30,50,250,70,elv_data,"-")
 
-#heat_map = revLoam(Reef,elv_data,arc_probs,100000,false)
+heat_map = revLoam(Reef,elv_data,arc_probs,100000,false)
 
 max = maximum(heat_map)
 
@@ -53,11 +53,11 @@ x_bounds, y_bounds = size(z)
 x = 1:x_bounds
 y = 1:y_bounds
 
-#heat_map = log.(heat_map .+ 1)
+heat_map = log.(heat_map .+ 1)
 
 fig=Plot([surface(x=x, y=y, z=z, surfacecolor=heat_map, 
         colorscale=colors.gist_earth, colorbar_thickness=25, colorbar_len=0.75
-        ), surface(x=x, y=y, z=-1*ones(size(z)), surfacecolor=heat_map,
+        ), surface(x=x, y=y, z=100*ones(size(z)), surfacecolor=heat_map,
                         colorscale=colors.gist_earth,
                         showscale=false)],
 Layout(width=600, height=600, font_size=10, scene_camera_eye=attr(x=1.6, y=1.6, z=1)))
